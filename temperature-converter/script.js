300585a8d9373d368dd1aea14231ba2a4af60326
Berkin Düz <mberkinduz@gmail.com>
2023-07-29 18:21:55 +0300

    Delete temperature-converter directory

--- temperature-converter/script.js
+++
@@ -1,9 +0,0 @@
-function convert() {
-  const degree = document.getElementById("convDegree").value || 1;
-  const typeSelection = document.getElementById("typeSelect").value;
-
-  let result =
-    typeSelection === "fahrenheit" ? (degree - 32) * 0.55 : degree * 1.8 + 32;
-
-  document.getElementById("convertResult").innerHTML = `${result} &degC`;
-}
